0ea643707add2cc5625d179dbf80268a298a2187
mosharof85 <68509780+mosharof85@users.noreply.github.com>
2024-08-20 22:46:01 +0600

    Create global url.php

---
+++ global url.php
@@ -0,0 +1,4 @@
+if ( !defined('WP_CLI') ) {
+    define( 'WP_SITEURL', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] );
+    define( 'WP_HOME',    $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] );
+}
