f675c3f6db9c981474589d812edb61e7572dbd39
mosharof85 <68509780+mosharof85@users.noreply.github.com>
2024-10-04 21:36:42 +0600

    Create catch ajax.php

---
+++ catch ajax.php
@@ -0,0 +1,15 @@
+jQuery(document).ajaxComplete(function(event, xhr, settings) {
+                if(settings.data.includes('action=pld_post_undo_ajax_action') ||
+                    settings.data.includes('action=pld_post_ajax_action')){
+                    let response = (JSON.parse(xhr.responseText));
+
+                    if(response.latest_count ===1){
+                        document.querySelector('.pld-like-count-wrap.pld-count-wrap').classList.remove('disabled')
+                        document.querySelector('.pld-like-wrap .fa.fa-heart-o').className = 'fas fa-heart'
+                    }
+                    if(response.latest_count ===0){
+                        document.querySelector('.pld-like-count-wrap.pld-count-wrap').classList.add('disabled')
+                        document.querySelector('.pld-like-wrap .fas.fa-heart').className = 'fa fa-heart-o'
+                    }
+                }
+            });
