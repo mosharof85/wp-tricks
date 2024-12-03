27f9ee10ee19ab5957f096e3b34350bb2c98a177
mosharof85 <68509780+mosharof85@users.noreply.github.com>
2024-12-04 03:22:58 +0600

    Update fix-language issue.php

--- fix-language issue.php
+++ fix-language issue.php
@@ -2,3 +2,6 @@ add_action('init', function() {
 	unload_textdomain('woocommerce');
 	load_textdomain('woocommerce', WP_LANG_DIR . '/plugins/woocommerce-de_DE.mo');
 }, 20);
+
+
+add_filter( 'doing_it_wrong_trigger_error', '__return_false' );
