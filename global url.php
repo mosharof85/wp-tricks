fc08256c151ff3b6bbb8ffa509d05a7aa9aa0fb8
mosharof85 <68509780+mosharof85@users.noreply.github.com>
2025-08-06 01:29:53 +0600

    Update global url.php

--- global url.php
+++ global url.php
@@ -2,3 +2,12 @@ if ( !defined('WP_CLI') ) {
     define( 'WP_SITEURL', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] );
     define( 'WP_HOME',    $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] );
 }
+
+
+
+
+$scheme = isset($_SERVER['REQUEST_SCHEME']) ? $_SERVER['REQUEST_SCHEME'] : 'http';
+$host   = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost';
+
+define('WP_HOME', $scheme . '://' . $host);
+define('WP_SITEURL', $scheme . '://' . $host);
